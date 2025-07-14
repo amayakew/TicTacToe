70d6128dd2ee801a5b2b68105a2256b1b78c4eaa
amaya <amaya.kew@gmail.com>
2025-07-14 16:17:45 +0300

    add log area

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import { useState } from "react"
 import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
+import Log from "./components/Log";
+
+const deriveActivePlayer = (gameTurns) => {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+};
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
 
-  const handleSelectSquare = () => {
-    setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
+  // let currentPlayer = "X";
+
+  // if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+  //   currentPlayer = "O";
+  // }
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    // setActivePlayer((prevActivePlayer) => prevActivePlayer === "X" ? "O" : "X");
+    setGameTurns((prevTurns) => {
+      // let currentPlayer = "X";
+
+      // if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      //   currentPlayer = "O";
+      // }
+
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        {
+          square: {row: rowIndex, col: colIndex},
+          player: currentPlayer,
+        },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   };
 
   return (
@@ -16,8 +55,9 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
       </div>
+      <Log turns={gameTurns}/>
     </main>
   )
 }
